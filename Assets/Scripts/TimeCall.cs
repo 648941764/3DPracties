0217f8d1fa9de8b0a2a1fe0a059b9edd06937bab
EEEEEEEEEEEEX <1165740681@qq.com>
2023-07-25 23:21:29 +0800

    1

--- Assets/Scripts/TimeCall.cs
+++ Assets/Scripts/TimeCall.cs
@@ -2,20 +2,77 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using static TimerController;
 
 public class TimeCall : MonoBehaviour
 {
-    public Text textUi;
-    public Button StartBtn;
-    public Button PauseBtn;
-    public Button ResetBtn;
-    public TimerController t1;
-    private float currenTime;
+    public int count = 100;
+
+    Button prefab;
+    int i;
+
+    Dictionary<Button, Timer> timerDic = new Dictionary<Button, Timer>();
+    Dictionary<Button, Text> textDic = new Dictionary<Button, Text>();
+
+    private void Awake()
+    {
+        prefab = Resources.Load<Button>("Button");
+    }
+
     private void Start()
     {
-        t1 = new TimerController(10f, 1f, textUi, "一号");
-        //currenTime = TimerController.instance._currentTime;
-        textUi.text = currenTime.ToString("F2");
+        i = -1;
+        while (++i < count)
+        {
+            Button btn = Instantiate(prefab, transform);
+            Text text = btn.GetComponentInChildren<Text>();
+            float duration = Random.Range(1f, 15f);
+            float speed = 0.001f; Random.Range(0.001f, 2f);
+            text.text = duration.ToString("f2") + "-" + speed;
+            Timer timer = TimerController.Instance.StartTimer(
+                duration,
+                speed,
+                currentTime => text.text = currentTime.ToString("f2"),
+                () => text.text = "完成"
+            );
+            timerDic.Add(btn, timer);
+            textDic.Add(btn, text);
+            btn.onClick.AddListener(() =>
+            {
+                Timer current = timerDic[btn];
+                if (current.IsComplete)
+                {
+                    current.Reset();
+                    textDic[btn].text = "重置";
+                }
+                else
+                {
+                    current.Pause();
+                    if (!current._isRunning)
+                    {
+                        textDic[btn].text = "暂停";
+                    }
+                }
+            });
+        }
+    }
 
+    private void OnGUI()
+    {
+        if (GUILayout.Button("ALL", GUILayout.Width(200), GUILayout.Height(100)))
+        {
+            foreach (var item in timerDic.Keys)
+            {
+                item.onClick.Invoke();
+            }
+        }
+        if (GUILayout.Button("Reset", GUILayout.Width(200), GUILayout.Height(100)))
+        {
+            foreach (var item in timerDic.Keys)
+            {
+                timerDic[item].Reset();
+                textDic[item].text = "Reset";
+            }
+        }
     }
 }
--- Assets/Scripts/TimerController.cs
+++ Assets/Scripts/TimerController.cs
@@ -1,135 +1,118 @@
-using System.Collections;
 using System.Collections.Generic;
-using System.IO.IsolatedStorage;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
 
 public class TimerController : MonoBehaviour
 {
+    private static TimerController _instance;
+    public static TimerController Instance => _instance;
 
-    public static TimerController instance;
+    List<Timer> _timerToAddList = new List<Timer>();
+    //List<Timer> _timerToRemoveList = new List<Timer>();
+    List<Timer> _timerList = new List<Timer>();
 
-    public static TimerController Instance
-    {
-        get { return instance; }
-    }
     private void Awake()
     {
-        instance = this;
-    }
-    //public Text uiText;
-    //public Text uiText2;
-    //public Button startBtn, pauseBtn, resetBtn;
-    //private TimeControll t1,t2;
-
-    private void Start()
-    {
-        //t2 = new TimeControll(12, 2, uiText2,"计时器二号");
-
-        //t1 = new TimeControll(10, 1.00f,uiText,"计时器一号");
-        //uiText.text = string.Format(t1._currentTime.ToString("F2"));
-        //uiText2.text = string.Format(t1._currentTime.ToString("F2"));
-        //startBtn.onClick.AddListener(OnstartBtnClick);
-        //pauseBtn.onClick.AddListener(OnPauseBtnClick);
-        //resetBtn.onClick.AddListener(OnResetBtnClick);
+        if (_instance != null)
+        {
+            Destroy(gameObject);
+        }
+        _instance = this;
     }
 
     private void Update()
     {
-
-    }
-    //public void OnstartBtnClick()
-    //{
-    //    if ( t1.isTimerRunning == false)
-    //    {
-    //        //StartCoroutine(t1.Timer());
-    //        //StartCoroutine(t2.Timer());
-    //        t1.Begin();
-    //        t2.Begin();
-    //        t1.isTimerRunning = true;
-    //        t2.isTimerRunning = true;
-    //    }
-    //}
-
-    //public void OnPauseBtnClick()
-    //{
-    //    //StopCoroutine(t1.Timer());
-    //    //StopCoroutine(t2.Timer());
-    //    t1.Pause();
-    //    t2.Pause();
-    //}
-
-    //public void OnResetBtnClick()
-    //{
-    //    t1.Reset();
-    //    t2.Reset();
-    //    uiText.text = string.Format(t1._currentTime.ToString("F2"));
-    //    uiText2.text = string.Format(t2._currentTime.ToString("F2"));
-    //    t1.isTimerRunning = false;
-    //    t2.isTimerRunning = false;
-    //}
-
-    private Text _timerText;
-    private float _duration;
-    private float _currentTime;
-    private bool _isRunning;
-    private float _speed;
-    private string _method;
-    private bool isTimerRunning = false;
-    public TimerController(float duration, float speed, Text timerText, string method)
-    {
-        _speed = speed;
-        _duration = duration;
-        _currentTime = _duration;
-        _isRunning = false;
-        _timerText = timerText;
-        _method = method;
-        _timerText.text = string.Format(duration.ToString("F2"));
-    }
-
-    public void Begin()
-    {
-        if (_currentTime <= 0)
+        int i = -1;
+        if (_timerToAddList.Count > 0)
         {
-            _currentTime = _duration;
+            _timerList.AddRange(_timerToAddList);
+            _timerToAddList.Clear();
         }
-        _isRunning = true;
-    }
-
-    public void Pause()
-    {
-        _isRunning = !_isRunning;
-        if (_currentTime <= 0)
+        while (++i < _timerList.Count)
         {
-            _timerText.text = string.Format("倒计时结束");
+            _timerList[i].Tick();
+            //if (timer.IsComplete) { _timerToRemoveList.Add(timer); }
         }
-        //else
+        //if (_timerToRemoveList.Count > 0)
         //{
-        //    isRunning = !isRunning;
+        //    i = -1;
+        //    while (++i < _timerToRemoveList.Count)
+        //    {
+        //        _timerList.Remove(_timerToRemoveList[i]);
+        //    }
+        //    _timerToRemoveList.Clear();
         //}
     }
 
-    public void Reset()
+    public Timer StartTimer(float duration, float speed, Action<float> onTick, Action onComplete, bool autoBegin = false)
     {
-        _isRunning = false;
-        _currentTime = _duration;
-    }
-
-    public void Timer()
-    {
-        _currentTime = _duration;
-        _isRunning = true;
-        if (_currentTime >= 0 && _isRunning == true)
+        Timer timer = new Timer()
         {
-            _currentTime -= _speed * Time.deltaTime;
+            _duration = duration,
+            _speed = speed,
+            _currentTime = duration,
+            _onTick = onTick,
+            _onComplete = onComplete,
+        };
+        _timerToAddList.Add(timer);
+        if (autoBegin)
+        {
+            timer.Begin();
         }
-        _timerText.text = string.Format("倒计时结束");
-        _isRunning = false;
+        return timer;
     }
 
-    public string Method()
+    public class Timer
     {
-        return _method;
-    }
+        public float _duration;
+        public float _speed;
+        public float _currentTime;
+        public Action<float> _onTick;
+        public Action _onComplete;
+        public bool _isRunning;
 
+        private float _elapsed;
+
+        public bool IsComplete => _currentTime <= 0f;
+
+        public void Begin()
+        {
+            //if (_currentTime <= 0)
+            //{
+            //    _currentTime = _duration;
+            //}
+            _isRunning = true;
+        }
+
+        public void Pause()
+        {
+            _isRunning = !_isRunning;
+        }
+
+        public void Reset()
+        {
+            _isRunning = false;
+            _currentTime = _duration;
+        }
+
+        public void Tick()
+        {
+            if (!_isRunning) { return; }
+
+            _elapsed += Time.deltaTime;
+            while (_elapsed >= _speed)
+            {
+                _elapsed -= _speed;
+                _currentTime -= _speed;
+                _onTick?.Invoke(_currentTime);
+            }
+
+            if (_currentTime <= 0f)
+            {
+                _isRunning = false;
+                _onComplete?.Invoke();
+            }
+        }
+    }
 }
